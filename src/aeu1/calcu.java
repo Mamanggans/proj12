6695f50e9c632b41c407eda22a20816241e84525
ferdiansyah <ferdikings03@gmail.com>
2020-01-15 16:38:14 +0700

    sayang

---
+++ src/aeu1/calcu.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aeu1;
+
+import jdk.nashorn.internal.parser.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class calcu {
+ 
+ public static void main (String[] args){
+ 
+ }
+ 
+}
---
+++ src/aeu1/penjumlahan.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aeu1;
+
+/**
+ *
+ * @author USER
+ */
+public class penjumlahan {
+    
+
+  public static void main(String[] args)
+  {
+       int number1 = 15, number2 = 10;
+       char chr1 = 'a', chr2 = 'b';
+            String str1 = "Hello ", str2 = "World";
+      System.out.println("15 + 10 = " + (15 + 10));
+      System.out.println("15 - 10 = " + (15 - 10));
+      System.out.println("15 * 10 = " + (15 * 10));
+      System.out.println("15 / 10 = " + (15.0 / 10));
+      System.out.println("15 % 10 = " + (15 % 10));
+      System.out.println("");
+      System.out.println("number1 (before ++) = " + number1);
+      System.out.println("number1++ = " + (number1++));
+      System.out.println("number1 (after ++) = " + number1);
+      System.out.println("");
+      System.out.println("number2 (before ++) = " + number2);
+      System.out.println("++number2 = " + (++number2));
+      System.out.println("number2 (after ++) = " + number2);
+      System.out.println("");
+      System.out.println("chr1 + chr2 = " + (chr1 + chr2));
+      System.out.println("str1 + str2 = " + (str1 + str2));
+  }
+ 
+}
